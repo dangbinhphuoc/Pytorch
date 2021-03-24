d84365510576b5cb93592283c896add65291645a
Boon Real Kua <dangbinhphuoc.it@gmail.com>
2021-03-24 14:10:59 +0700

    sửa thư mục

--- Autograd/2.autograd.py
+++ Autograd/2.autograd.py
@@ -1,9 +1,7 @@
 import torch
 
-# x
-# cách 1:
 x = torch.ones(2, 2, requires_grad=True)
-# cách 2:
+# hoặc
 # x = torch.ones(2, 2)
 # x.requires_grad_(True)
 print(x)
@@ -11,6 +9,7 @@ print(x)
 # y = x + 2
 y = x + 2
 print(y)
+# .grad_fn được dùng để theo dõi quá trình tính toán
 print(y.grad_fn)
 
 # z = 3*(x+2)^2
@@ -24,8 +23,10 @@ print(out)
 # d(out)/dx = (3/2)*(x+2)
 # thực hiện backprop bằng lệnh .backward()
 out.backward()
-# kết quả sẽ được lưu tại .grad
+# kết quả đạo hàm sẽ được lưu tại .grad
 print(x.grad)
 
+from torchviz import make_dot
+print(make_dot(out))
 
-
+    
