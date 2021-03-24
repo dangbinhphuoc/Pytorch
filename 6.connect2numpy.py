416fbf6e49683963aeb7cdb96a79cfb3d17cf105
Boon Real Kua <dangbinhphuoc.it@gmail.com>
2021-03-24 08:46:59 +0700

    xoá nội dung cũ thay nội dung mới tường minh hơn

--- 6.connect2numpy.py
+++ 6.connect2numpy.py
@@ -1,28 +1,26 @@
 import torch
 import numpy as np
 
-# torch -> numpy
-x = torch.ones(5)
-print(type(x))
+# chuyển tensor thành numpy
+x = torch.tensor([1,2,3])
+x_np = x.numpy()
 
-y = x.numpy()
-print(type(y))
+# Torch tensor và Numpy array sẽ dùng chung vùng nhớ 
+# nên khi thay đổi torch tensor thì Numpy array cũng thay đổi
+x[1] = 0
+print(x) # output: [1, 0, 3]
+print(x_np) # output: [1, 0, 3]
 
-print()
-print(x)
-print(y)
+# Nếu tensor được lưu trên GPU thì không thể chuyển trực tiếp tensor sang Numpy array được,
+# Cần copy nội dung của tensor sang CPU trước rồi mới chuyển sang Numpy array. 
+# Do đó 2 biến trên gpu và np không dùng chung vùng nhớ và sửa 1 biến không ảnh hưởng biến còn lại.
+x_gpu = torch.tensor([1, 2, 3], device='cuda')
+x_np = x_gpu.numpy() # Error
+x_np = x_gpu.cpu().numpy() # ok
+x_gpu[1] = 0 
+print(x_gpu) # output: [1, 0, 3]
+print(x_np) # output: [1, 2, 3]
 
-print()
-x.add_(1)
-print(x)
-print(y)
-
-print()
-
-# numpy -> torch
-x = np.ones(5)
-y = torch.from_numpy(x)
-print(type(x))
-print(type(y))
-print(x)
-print(y)
+# chuyển từ numpy sang tensor
+x_np = np.array([1, 2, 3])
+x_cpu = torch.from_numpy(x_np)
